97e87acf1331a80a7b8d23622bc85e336c31e1db
EdwardBock <edward.bock@palasthotel.de>
2023-04-25 14:36:39 +0200

    database automatic update

--- public/classes/Updates.php
+++ public/classes/Updates.php
@@ -13,7 +13,7 @@ class Updates extends Update {
 	}
 
 	function getVersion(): int {
-		return 1;
+		return 2;
 	}
 
 	function setCurrentVersion( int $version ) {
@@ -30,6 +30,17 @@ class Updates extends Update {
 		$wpdb->query("ALTER TABLE $table CHANGE changed_data_value_old changed_data_value_old LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT ''" );
 		$wpdb->query("ALTER TABLE $table CHANGE changed_data_value_new changed_data_value_new LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT ''" );
 		$wpdb->query("ALTER TABLE $table CHANGE variables variables LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT ''" );
+	}
+
+	function update_2(){
+		global $wpdb;
+		$table = Plugin::instance()->database->tableLogItems;
+		$logsTable = Plugin::instance()->database->tableLogs;
+		$tableDeprecated = $table."_deprecated";
+		$wpdb->query("RENAME TABLE $table to $tableDeprecated;");
+		Plugin::instance()->database->createTables();
+		$wpdb->query("INSERT INTO $table SELECT * FROM $tableDeprecated WHERE process_id IN (SELECT id FROM $logsTable);");
+		$wpdb->query("DROP TABLE $tableDeprecated");
 	}
 
 }
